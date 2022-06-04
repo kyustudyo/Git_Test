adeb1f2369de7615a4939d8431557115c0632e30
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-06-04 22:15:26 +0900

    Initial Commit

---
+++ Git_Test/Git_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_TestApp.swift
+//  Git_Test
+//
+//  Created by Hankyu Lee on 2022/06/04.
+//
+
+import SwiftUI
+
+@main
+struct Git_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
